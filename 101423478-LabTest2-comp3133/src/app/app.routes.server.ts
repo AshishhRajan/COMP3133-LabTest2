513499090a569d4f1088d3622e3f3dac2874b49c
AshishhRajan <152151880+AshishhRajan@users.noreply.github.com>
2025-04-02 19:18:58 -0400

    Delete 101423478-LabTest2-comp3133 directory

--- 101423478-LabTest2-comp3133/src/app/app.routes.server.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { MissionlistComponent } from './components/missionlist/missionlist.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/missions', pathMatch: 'full' },
-  { path: 'missions', component: MissionlistComponent },
-  { path: '**', redirectTo: '/missions' }
-];
